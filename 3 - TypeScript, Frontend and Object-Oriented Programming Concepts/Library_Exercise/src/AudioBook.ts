c8ff7bde111e19e14f3b84f398fabd3390c88008
Nikki Mae Evers <37475750+nikkimae@users.noreply.github.com>
2024-07-18 14:41:23 +0200

    Add Livecoded solution Parts 1, 2 and 3

---
+++ 3 - TypeScript, Frontend and Object-Oriented Programming Concepts/Library_Exercise/src/AudioBook.ts
@@ -0,0 +1,47 @@
+import {DigitalBook} from './DigitalBook'
+import { AudioBookDetails, DigitalBookDetails } from './Types';
+
+// Define a new class AudioBook that extends DigitalBook.
+export class AudioBook extends DigitalBook {
+    // Add properties duration (number, representing the duration in minutes) and narrator (string).
+    private _duration: number;
+    private _narrator: string
+
+    constructor(audioBookDetails: AudioBookDetails){
+        super(audioBookDetails);
+        this._duration = audioBookDetails.duration;
+        this._narrator = audioBookDetails.narrator;
+    }
+
+    // Add appropriate getters and setters for duration and narrator.
+    // get duration
+    get duration(): number {
+        return this._duration;
+    }
+    // set duration
+    set duration(newDuration: number){
+        this._duration = newDuration
+    }
+    // get narrator
+    get narrator(): string {
+        return this._narrator;
+    }
+    // set narrator
+    set narrator(newNarrator: string){
+        this._narrator = newNarrator
+    }
+
+    // Override the getBookDetails() method to include the narrator and duration in the details.
+    getBookDetails(): AudioBookDetails {
+        // Get book details from super class
+        const details = super.getBookDetails()
+        // return bookdetails with duration and narrator
+        return {
+            ...details,
+            duration: this._duration,
+            narrator: this._narrator
+        }
+    }
+}
+
+
