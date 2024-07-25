1c45e1e49d74389bfb352f69dc5bf281b7babfbe
ahzia <ah.ziayosfi@gmail.com>
2024-07-25 09:37:39 +0200

    adding user to posts

--- 3 - TypeScript, Frontend and Object-Oriented Programming Concepts/Week 12/1,2 NextJS introduction/my-app/src/app/page.tsx
+++ 3 - TypeScript, Frontend and Object-Oriented Programming Concepts/Week 12/1,2 NextJS introduction/my-app/src/app/page.tsx
@@ -5,21 +5,45 @@ const posts: IPost[] = [
   {
     id: 1,
     title: "Post 1",
-    content: "asdasdasd"
+    content: "asdasdasd",
+    creationDate: new Date(),
+    user: {
+      name: "asdasd",
+      surname: "asdasd",
+      age: 21,
+    }
   },
   {
     id: 2,
     title: "Post 2",
-    content: "asdasdasd"
+    content: "asdasdasd",
+    creationDate: new Date(),
+    user: {
+      name: "asdasd",
+      surname: "asdasd",
+      age: 21,
+    }
   },
   {
     id: 3,
     title: "Post 3",
-    content: "asdasdasd"
+    content: "asdasdasd",
+    creationDate: new Date(),
+    user: {
+      name: "abcd",
+      surname: "asdasd",
+      age: 21,
+    }
   },
   {
-    id: 3,
-    title: "Post 3"
+    id: 4,
+    title: "Post 3",
+    creationDate: new Date(),
+    user: {
+      name: "John",
+      surname: "asdasd",
+      age: 21,
+    }
   }
 ]
 
@@ -33,6 +57,10 @@ export default function Home() {
             <p>post :{post.id}</p>
             <p>Title: {post.title}</p>
             <p>Content: {post.content ? post.content : 'No Content'}</p>
+            <div>
+              <p>Created by: {post.user.name} {post.user.surname}</p>
+              <p>User Age: {post.user.age}</p>
+            </div>
           </>
         ))
 
--- 3 - TypeScript, Frontend and Object-Oriented Programming Concepts/Week 12/1,2 NextJS introduction/my-app/src/types/types.ts
+++ 3 - TypeScript, Frontend and Object-Oriented Programming Concepts/Week 12/1,2 NextJS introduction/my-app/src/types/types.ts
@@ -2,6 +2,15 @@ export interface IPost {
     id: number,
     title: string,
     content?: string,
+    creationDate?: Date,
+    user: User;
+}
+
+
+export interface User {
+    name: string;
+    surname: string;
+    age: number;
 }
 
 export interface IPostParams {
